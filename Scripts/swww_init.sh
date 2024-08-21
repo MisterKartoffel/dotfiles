7b180fa11aced0cda9718d5720c0c6f334764aa7
Your Name <felipesdrs@hotmail.com>
2024-08-21 15:56:42 -0300

    Changed method from copy to hardlink

--- Scripts/swww_init.sh
+++ Scripts/swww_init.sh
@@ -10,6 +10,6 @@
 WALLPAPER=$(find /home/felipe/Wallpapers -type f | shuf -n 1)
 
 swww img $WALLPAPER                                                                                 # Set new wallpaper
-cp --remove-destination $WALLPAPER /usr/share/sddm/themes/sugar-candy/Backgrounds/background.png	# Update SDDM background to new wallpaper
-cp --remove-destination $WALLPAPER /home/felipe/.config/hypr/background.png				            # Update hyprlock background to new wallpaper
+ln -f $WALLPAPER /usr/share/sddm/themes/sugar-candy/Backgrounds/background.png	                    # Update SDDM background to new wallpaper
+ln -f $WALLPAPER /home/felipe/.config/hypr/background.png				                            # Update hyprlock background to new wallpaper
 killall waybar && nohup waybar &                                									# Kill and restart waybar
