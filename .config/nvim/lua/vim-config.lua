3ced5891653f94f6694f62726c751bd5b4d60785
Your Name <felipesdrs@hotmail.com>
2024-08-21 14:48:54 -0300

    Moved vim-maximizer keybind to vim-config.lua, refactored local keybind variable

--- .config/nvim/lua/vim-config.lua
+++ .config/nvim/lua/vim-config.lua
@@ -5,29 +5,32 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.g.mapleader = " "
 
-local keymap = vim.keymap -- Saves keystrokes
+local keybind = vim.keymap.set -- Saves keystrokes
 
 -- Neotree keybindings
-keymap.set("n", "<leader>ee", ":Neotree filesystem toggle left<CR>", { desc = "Toggle tree on/off" })
-keymap.set("n", "<leader>ef", ":Neotree focus<CR>", { desc = "Regain focus on tree" })
+keybind("n", "<leader>ee", ":Neotree filesystem toggle left<CR>", { desc = "Toggle tree on/off" })
+keybind("n", "<leader>ef", ":Neotree focus<CR>", { desc = "Regain focus on tree" })
 
 -- LSP keybindings
-keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "Display info about symbol under cursor" })
-keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "Jump to definition for symbol under cursor" })
-keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Displays code actions for annotation under cursor" })
+keybind("n", "<leader>ch", vim.lsp.buf.hover, { desc = "Display info about symbol under cursor" })
+keybind("n", "<leader>cd", vim.lsp.buf.definition, { desc = "Jump to definition for symbol under cursor" })
+keybind("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Displays code actions for annotation under cursor" })
 
 -- None-LS keybindings
-keymap.set("n", "<leader>nf", vim.lsp.buf.format, { desc = "Autoformat current file" })
+keybind("n", "<leader>af", vim.lsp.buf.format, { desc = "Autoformat current file" })
 
 -- Telescope keybindings
-keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Fuzzy find files in CWD" })
-keymap.set("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
-keymap.set("n", "<leader>fs", ":Telescope live_grep<CR>", { desc = "Find string in CWD" })
-keymap.set("n", "<leader>fc", ":Telescope grep_string<CR>", { desc = "Find string under cursor in CWD" })
+keybind("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Fuzzy find files in CWD" })
+keybind("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+keybind("n", "<leader>fs", ":Telescope live_grep<CR>", { desc = "Find string in CWD" })
+keybind("n", "<leader>fc", ":Telescope grep_string<CR>", { desc = "Find string under cursor in CWD" })
 
 -- WhichKey keybindings
-keymap.set("n", "<leader>?", function() require("which-key").show({ global = false }) end, { desc = "Show local keymaps" })
+keybind("n", "<leader>?", function() require("which-key").show({ global = true }) end, { desc = "Show local keymaps" })
+
+-- Vim-maximizer keybindings
+keybind("n", "<C-w>m", ":MaximizerToggle<CR>", { desc = "Maximize/minimize a split" })
 
 -- Auto-session keybindings
-keymap.set("n", "<leader>wr", ":SessionRestore<CR>", { desc = "Restore session for CWD" })
-keymap.set("n", "<leader>ws", ":SessionSave<CR>", { desc = "Save session for CWD" })
+keybind("n", "<leader>wr", ":SessionRestore<CR>", { desc = "Restore session for CWD" })
+keybind("n", "<leader>ws", ":SessionSave<CR>", { desc = "Save session for CWD" })
