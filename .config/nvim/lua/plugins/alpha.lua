16d651109ef0b61fa102c9723bb9f7def450ec1e
Your Name <felipesdrs@hotmail.com>
2024-08-21 00:28:54 -0300

    Added plugin count and version number in alpha dashboard footer

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -26,15 +26,23 @@ return {
             [[                                                                       ]],
         }
         dashboard.section.buttons.val = {
-            dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-            dashboard.button( "f", "  > Find file", ":cd $HOME | Telescope find_files<CR>"),
-            dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
-            dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd $HOME/.config/nvim | :Neotree filesystem toggle left<CR>"),
-            dashboard.button( "q", "󰩈  > Quit NVIM", ":qa<CR>"),
+            dashboard.button( "e", "  ❱ New file" , ":ene <BAR> startinsert <CR>"),
+            dashboard.button( "f", "  ❱ Find file", ":cd $HOME | Telescope find_files<CR>"),
+            dashboard.button( "r", "  ❱ Recent"   , ":Telescope oldfiles<CR>"),
+            dashboard.button( "s", "  ❱ Settings" , ":e $MYVIMRC | :cd $HOME/.config/nvim | :Neotree filesystem toggle left<CR>"),
+            dashboard.button( "q", "󰩈  ❱ Quit NVIM", ":qa<CR>"),
         }
+        local function footer()
+            local total_plugins = require("lazy").stats().count
+            local version = vim.version()
+            local nvim_version_info = "   Neovim v" .. version.major .. "." .. version.minor .. "." .. version.patch
+
+            return "  " .. total_plugins .. " plugins" .. nvim_version_info
+        end
+        dashboard.section.footer.val = footer()
         alpha.setup(dashboard.opts)
         vim.cmd([[
             autocmd FileType alpha setlocal nofoldenable
-            ]])
+        ]])
     end,
 }
