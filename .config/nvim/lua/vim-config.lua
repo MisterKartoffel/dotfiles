e9a649331662559abd8dfb03162c802b5266d2c9
Your Name <felipesdrs@hotmail.com>
2024-08-21 15:02:37 -0300

    Re-ordered keybindings alphabetically, adopted same filename as <plugin>.lua for easy grepping

--- .config/nvim/lua/vim-config.lua
+++ .config/nvim/lua/vim-config.lua
@@ -1,3 +1,7 @@
+
+-- █▀▀ █░░ █▀█ █▄▄ ▄▀█ █░░   █▀ █▀▀ ▀█▀ ▀█▀ █ █▄░█ █▀▀ █▀
+-- █▄█ █▄▄ █▄█ █▄█ █▀█ █▄▄   ▄█ ██▄ ░█░ ░█░ █ █░▀█ █▄█ ▄█
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
@@ -5,16 +9,24 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.g.mapleader = " "
 
+
+-- █▄▀ █▀▀ █▄█ █▄▄ █ █▄░█ █▀▄ █ █▄░█ █▀▀ █▀
+-- █░█ ██▄ ░█░ █▄█ █ █░▀█ █▄▀ █ █░▀█ █▄█ ▄█
+
 local keybind = vim.keymap.set -- Saves keystrokes
 
--- Neotree keybindings
-keybind("n", "<leader>ee", ":Neotree filesystem toggle left<CR>", { desc = "Toggle tree on/off" })
-keybind("n", "<leader>ef", ":Neotree focus<CR>", { desc = "Regain focus on tree" })
+-- Auto-session keybindings
+keybind("n", "<leader>wr", ":SessionRestore<CR>", { desc = "Restore session for CWD" })
+keybind("n", "<leader>ws", ":SessionSave<CR>", { desc = "Save session for CWD" })
 
 -- LSP keybindings
 keybind("n", "<leader>ch", vim.lsp.buf.hover, { desc = "Display info about symbol under cursor" })
 keybind("n", "<leader>cd", vim.lsp.buf.definition, { desc = "Jump to definition for symbol under cursor" })
 keybind("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Displays code actions for annotation under cursor" })
+
+-- Neo-tree keybindings
+keybind("n", "<leader>ee", ":Neotree filesystem toggle left<CR>", { desc = "Toggle tree on/off" })
+keybind("n", "<leader>ef", ":Neotree focus<CR>", { desc = "Regain focus on tree" })
 
 -- None-LS keybindings
 keybind("n", "<leader>af", vim.lsp.buf.format, { desc = "Autoformat current file" })
@@ -25,12 +37,8 @@ keybind("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Fuzzy find recen
 keybind("n", "<leader>fs", ":Telescope live_grep<CR>", { desc = "Find string in CWD" })
 keybind("n", "<leader>fc", ":Telescope grep_string<CR>", { desc = "Find string under cursor in CWD" })
 
--- WhichKey keybindings
-keybind("n", "<leader>?", function() require("which-key").show({ global = true }) end, { desc = "Show local keymaps" })
-
 -- Vim-maximizer keybindings
 keybind("n", "<C-w>m", ":MaximizerToggle<CR>", { desc = "Maximize/minimize a split" })
 
--- Auto-session keybindings
-keybind("n", "<leader>wr", ":SessionRestore<CR>", { desc = "Restore session for CWD" })
-keybind("n", "<leader>ws", ":SessionSave<CR>", { desc = "Save session for CWD" })
+-- Which-key keybindings
+keybind("n", "<leader>?", function() require("which-key").show({ global = true }) end, { desc = "Show keymaps" })
