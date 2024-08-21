6e8f2dfb4f1f3d7be5ae07ba92d2d101fcb50d51
Your Name <felipesdrs@hotmail.com>
2024-08-21 00:28:22 -0300

    Redid comment spacing

--- Scripts/swww_init.sh
+++ Scripts/swww_init.sh
@@ -9,7 +9,7 @@
 
 WALLPAPER=$(find /home/felipe/Wallpapers -type f | shuf -n 1)
 
-swww img $WALLPAPER											# Set new wallpaper
+swww img $WALLPAPER                                                                                 # Set new wallpaper
 cp --remove-destination $WALLPAPER /usr/share/sddm/themes/sugar-candy/Backgrounds/background.png	# Update SDDM background to new wallpaper
-cp --remove-destination $WALLPAPER /home/felipe/.config/hypr/background.png				# Update hyprlock background to new wallpaper
-killall waybar && nohup waybar &									# Kill and restart waybar
+cp --remove-destination $WALLPAPER /home/felipe/.config/hypr/background.png				            # Update hyprlock background to new wallpaper
+killall waybar && nohup waybar &                                									# Kill and restart waybar
