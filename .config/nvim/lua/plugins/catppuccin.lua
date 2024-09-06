d5b4e4c9d90c0fddf791cfcea74f510253c5cce4
Your Name <felipesdrs@hotmail.com>
2024-09-06 18:50:31 -0300

    Added catppuccin integrations to other plugins

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -2,7 +2,21 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
+
 	config = function()
-		vim.cmd.colorscheme("catppuccin-mocha")
-	end,
+        local catppuccin = require("catppuccin")
+        vim.cmd.colorscheme("catppuccin")
+
+        catppuccin.setup({
+            flavour = "mocha",
+            integrations = {
+                mason = true,
+                neotree = true,
+                cmp = true,
+                treesitter = true,
+                telescope = true,
+                which_key = true,
+            },
+        })
+    end
 }
