71a1c6a2bfed0f9372eb8c98a4c447a61f1cff2b
Your Name <felipesdrs@hotmail.com>
2024-09-07 01:39:15 -0300

    Simplified through opts to remove config function

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,20 +5,15 @@ return {
         "BufNewFile",
     },
     build = ":TSUpdate",
-
-    config = function()
-        local config = require("nvim-treesitter.configs")
-
-        config.setup({
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true },
-            vim.filetype.add({
-                pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
-            }),
-        })
-    end,
+    opts = {
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+        vim.filetype.add({
+            pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+        }),
+    },
 }
