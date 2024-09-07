ff0bbe06f90d0b999ce84f8e2f97164ecc0dbc41
Your Name <felipesdrs@hotmail.com>
2024-09-07 01:36:38 -0300

    Split LSP plugin files

---
+++ .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -0,0 +1,24 @@
+return {
+    "neovim/nvim-lspconfig",
+    event = {
+        "BufReadPre",
+        "BufNewFile",
+    },
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        { "antosha417/nvim-lsp-file-operations", config = true, },
+    },
+
+    config = function()
+        local lspconfig = require("lspconfig")
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        lspconfig.lua_ls.setup({ capabilities = capabilities })
+        lspconfig.bashls.setup({ capabilities = capabilities })
+        lspconfig.yamlls.setup({ capabilities = capabilities })
+        lspconfig.hyprls.setup({ capabilities = capabilities })
+        lspconfig.ts_ls.setup({ capabilities = capabilities })
+        lspconfig.taplo.setup({ capabilities = capabilities })
+        lspconfig.biome.setup({ capabilities = capabilities })
+    end,
+}
