6e392e1cf0f8420db5f6dea894a674fe6b01f096
Your Name <felipesdrs@hotmail.com>
2024-09-10 01:07:23 -0300

    Changed catppuccin config style
    Moved colorscheme declaration to ../core/options.lua, allowing for
    catppuccin.lua to be implicitly configured by an opts table.

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -7,6 +7,6 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
+vim.cmd("colorscheme catppuccin")
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 vim.g.mapleader = " "
-
--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -2,22 +2,15 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-
-    config = function()
-        local catppuccin = require("catppuccin")
-
-        catppuccin.setup({
-            flavour = "mocha",
-            integrations = {
-                cmp = true,
-                mason = true,
-                neotree = true,
-                telescope = true,
-                treesitter = true,
-                which_key = true,
-            },
-
-            vim.cmd.colorscheme("catppuccin")
-        })
-    end,
+    opts = {
+        flavour = "mocha",
+        integrations = {
+            cmp = true,
+            mason = true,
+            neotree = true,
+            telescope = true,
+            treesitter = true,
+            which_key = true,
+        },
+    },
 }
