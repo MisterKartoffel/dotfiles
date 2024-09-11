9f1d0e16c60eb8122950507d64cd928080dc7ece
Felipe Duarte <176643461+MisterKartoffel@users.noreply.github.com>
2024-09-11 01:16:33 -0300

    Added multiple new keymaps, added folding with per-source categories

--- .config/nvim/lua/core/keybindings.lua
+++ .config/nvim/lua/core/keybindings.lua
@@ -4,35 +4,64 @@
 
 local keybind = vim.keymap.set -- Saves keystrokes
 
--- Auto-session keybindings
-keybind("n", "<leader>wr", "<CMD>SessionRestore<CR>", { desc = "Restore session for CWD" })
-keybind("n", "<leader>ws", "<CMD>SessionSave<CR>", { desc = "Save session for CWD" })
-keybind("n", "<leader>wf", "<CMD>SessionSearch<CR>", { desc = "Search sessions" })
+-- Vanilla Neovim {{{
+    keybind("v", "J", ":m '>+1<CR>gv=gv")
+    keybind("v", "K", ":m '<-2<CR>gv=gv")
 
--- Lazygit keybindings
-keybind("n", "<leader>lg", "<CMD>LazyGit<CR>", { desc = "Open LazyGit" })
+    keybind("n", "<A-o>", "o<Esc>", { desc = "Create a new line below and stay in normal mode" })
+    keybind("n", "<A-O>", "O<Esc>", { desc = "Create a new line above and stay in normal mode" })
 
--- LSP and None-LS keybindings
-keybind("n", "<leader>ch", vim.lsp.buf.hover, { desc = "Display info about symbol under cursor" })
-keybind("n", "<leader>cd", vim.lsp.buf.definition, { desc = "Jump to definition for symbol under cursor" })
-keybind("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Displays code actions for annotation under cursor" })
-keybind("n", "<leader>cf", vim.lsp.buf.format, { desc = "Autoformat current file" })
+    keybind("n", "J", "mzJ`z", { desc = "Append line below while keeping cursor still" })
+    keybind("n", "<C-d>", "<C-d>zz", { desc = "Scroll half page down and center screen on cursor" })
+    keybind("n", "<C-u>", "<C-u>zz", { desc = "Scroll half page up and center screen on cursor" })
 
--- Neo-tree keybindings
-keybind("n", "<leader>ee", "<CMD>Neotree filesystem toggle left<CR>", { desc = "Toggle tree on/off" })
-keybind("n", "<leader>ef", "<CMD>Neotree focus<CR>", { desc = "Regain focus on tree" })
+    keybind("x", "<leader>p", [["_dP]], { desc = "Paste over selected string while keeping current register" })
 
--- Nvim-colorizer keybindings
-keybind("n", "<leader>cc", "<CMD>ColorizerToggle<CR>", { desc = "Toggle color highlighting of current buffer" })
+    keybind({"n", "v"}, "<leader>y", [["+y]], { desc = "Copy to system clipboard" })
+    keybind("n", "<leader>Y", [["+Y]], { desc = "Copy whole line to system clipboard" })
 
--- Telescope keybindings
-keybind("n", "<leader>ff", "<CMD>Telescope find_files<CR>", { desc = "Fuzzy find files in CWD" })
-keybind("n", "<leader>fr", "<CMD>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
-keybind("n", "<leader>fs", "<CMD>Telescope live_grep<CR>", { desc = "Find string in CWD" })
-keybind("n", "<leader>fc", "<CMD>Telescope grep_string<CR>", { desc = "Find string under cursor in CWD" })
+    keybind({"n", "v"}, "<leader>d", [["_d]], { desc = "Delete without copying" })
 
--- Vim-maximizer keybindings
-keybind("n", "<C-w>m", "<CMD>MaximizerToggle<CR>", { desc = "Maximize/minimize a split" })
+    keybind("n", "<leader><leader>", ":w<CR> :so<CR>", { desc = "Save and source current file" })
+-- }}}
 
--- Which-key keybindings
-keybind("n", "<leader>?", function() require("which-key").show({ global = true }) end, { desc = "Show keymaps" })
+-- Auto-session {{{
+    keybind("n", "<leader>wr", ":SessionRestore<CR>", { desc = "Restore session for CWD" })
+    keybind("n", "<leader>ws", ":SessionSave<CR>", { desc = "Save session for CWD" })
+    keybind("n", "<leader>wf", ":SessionSearch<CR>", { desc = "Search sessions" })
+-- }}}
+
+-- Lazygit {{{
+    keybind("n", "<leader>lg", ":LazyGit<CR>", { desc = "Open LazyGit" })
+-- }}}
+
+-- LSP and None-LS {{{
+    keybind("n", "<leader>ch", vim.lsp.buf.hover, { desc = "Display info about symbol under cursor" })
+    keybind("n", "<leader>cd", vim.lsp.buf.definition, { desc = "Jump to definition for symbol under cursor" })
+    keybind("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Displays code actions for annotation under cursor" })
+    keybind("n", "<leader>cf", vim.lsp.buf.format, { desc = "Autoformat current file" })
+-- }}}
+
+-- Neo-tree {{{
+    keybind("n", "<leader>ee", ":Neotree filesystem toggle left<CR>", { desc = "Toggle tree on/off" })
+    keybind("n", "<leader>ef", ":Neotree focus<CR>", { desc = "Regain focus on tree" })
+-- }}}
+
+-- Nvim-colorizer {{{
+    keybind("n", "<leader>cc", ":ColorizerToggle<CR>", { desc = "Toggle color highlighting of current buffer" })
+-- }}}
+
+-- Telescope {{{
+    keybind("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Fuzzy find files in CWD" })
+    keybind("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+    keybind("n", "<leader>fs", ":Telescope live_grep<CR>", { desc = "Find string in CWD" })
+    keybind("n", "<leader>fc", ":Telescope grep_string<CR>", { desc = "Find string under cursor in CWD" })
+-- }}}
+
+-- Vim-maximizer {{{
+    keybind("n", "<C-w>m", ":MaximizerToggle<CR>", { desc = "Maximize/minimize a split" })
+-- }}}
+
+-- Which-key {{{
+    keybind("n", "<leader>?", function() require("which-key").show({ global = true }) end, { desc = "Show keymaps" })
+-- }}}
