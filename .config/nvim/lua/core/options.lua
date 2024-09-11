63b5a08b9a370c381db19145e9a5ab282ae0cdd0
Felipe Duarte <176643461+MisterKartoffel@users.noreply.github.com>
2024-09-11 01:16:03 -0300

    Changed settings from vim.cmd() to vim.opt, added extra settings

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -2,11 +2,24 @@
 -- █▀▀ █░░ █▀█ █▄▄ ▄▀█ █░░   █▀ █▀▀ ▀█▀ ▀█▀ █ █▄░█ █▀▀ █▀
 -- █▄█ █▄▄ █▄█ █▄█ █▀█ █▄▄   ▄█ ██▄ ░█░ ░█░ █ █░▀█ █▄█ ▄█
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set number")
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.foldmethod = "marker"
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
 vim.cmd("colorscheme catppuccin")
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 vim.g.mapleader = " "
