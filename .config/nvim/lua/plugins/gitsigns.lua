8fcfaa6b6f74289ebb2502b12762735296916f25
Felipe Duarte <176643461+MisterKartoffel@users.noreply.github.com>
2024-09-13 00:03:13 -0300

    Changed some icons to increase standardization with other git apps, changed worktree to work with bare config repo, reduced delay before showing blame info

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,9 +5,22 @@ return {
         "BufNewFile",
     },
     opts = {
+        signs = {
+            add     = { text = '+' },
+            change  = { text = '~' },
+        },
+        signs_staged = {
+            add     = { text = '+' },
+            change  = { text = '~' },
+        },
         worktrees = {
-            toplevel = "$HOME",
-            gitdir = "$HOME/Dotfiles",
+            {
+                toplevel = vim.env.HOME,
+                gitdir = vim.env.HOME .. "/Dotfiles",
+            },
+        },
+        current_line_blame_opts = {
+            delay = 100,
         },
     },
 }
