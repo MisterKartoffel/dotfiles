3849812367fea72fc49bb1c72d0676a25fb2d6c3
Felipe Duarte <176643461+MisterKartoffel@users.noreply.github.com>
2024-09-18 17:09:19 -0300

    Updated to also generate flatpak app listing

--- Scripts/pkglist.sh
+++ Scripts/pkglist.sh
@@ -7,7 +7,7 @@ creationTime="$(date --rfc-3339="seconds")"
 pkgListDir="$HOME/.config/pkglists"
 
 if [ ! -e "$pkgListDir" ]; then
-    mkdir -p $pkgListDir/{aur,repositories,optional}
+    mkdir -p $pkgListDir/{aur,repositories,optional,flatpak}
     echo "Created $pkgListDir and subdirectories."
 elif [ -f "$pkgListDir" ]; then
     echo "Error: $pkgListDir exists as a file."
@@ -24,6 +24,9 @@ if [ -e "$pkgListDir" ]; then
     elif [ ! -e "$pkgListDir/optional" ]; then
         echo "Error: $pkgListDir/optional does not exist."
         exit -1
+    elif [ ! -e "$pkgListDir/flatpak" ]; then
+        echo "Error: $pkgListDir/flatpak does not exist."
+        exit -1
     else
         pacman -Qqten > $pkgListDir/repositories/"EXPLICIT-$creationTime.txt"
         echo "Created explicitly installed package listing at $pkgListDir/repositories/'EXPLICIT-$creationTime'"
@@ -33,6 +36,9 @@ if [ -e "$pkgListDir" ]; then
 
         comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) > $pkgListDir/optional/"OPTIONAL-$creationTime.txt"
         echo "Created installed optional dependency package listing at $pkgListDir/optional/'OPTIONAL-$creationTime.txt'"
+
+        flatpak list --app > $pkgListDir/flatpak/"FLATPAK-$creationTime.txt"
+        echo "Created installed flatpak apps listing at $pkgListDir/flatpak/'FLATPAK-$creationTime.txt'"
     fi
 fi
 
