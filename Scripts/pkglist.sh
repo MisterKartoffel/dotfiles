c751e3e0ce778f8d25642c8ae438737ce6c7abab
Felipe Duarte <176643461+MisterKartoffel@users.noreply.github.com>
2024-09-18 16:31:49 -0300

    Added script to organize and generate pacman installed package lists

---
+++ Scripts/pkglist.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# WARNING: THIS SCRIPT IS REFERENCED
+# BY FULL PATH IN /etc/pacman.d/hooks/96-pkglistgen_post.hook
+
+creationTime="$(date --rfc-3339="seconds")"
+pkgListDir="$HOME/.config/pkglists"
+
+if [ ! -e "$pkgListDir" ]; then
+    mkdir -p $pkgListDir/{aur,repositories,optional}
+    echo "Created $pkgListDir and subdirectories."
+elif [ -f "$pkgListDir" ]; then
+    echo "Error: $pkgListDir exists as a file."
+    exit -1
+fi
+
+if [ -e "$pkgListDir" ]; then
+    if [ ! -e "$pkgListDir/aur" ]; then
+        echo "Error: $pkgListDir/aur does not exist."
+        exit -1
+    elif [ ! -e "$pkgListDir/repositories" ]; then
+        echo "Error: $pkgListDir/repositories does not exist."
+        exit -1
+    elif [ ! -e "$pkgListDir/optional" ]; then
+        echo "Error: $pkgListDir/optional does not exist."
+        exit -1
+    else
+        pacman -Qqten > $pkgListDir/repositories/"EXPLICIT-$creationTime.txt"
+        echo "Created explicitly installed package listing at $pkgListDir/repositories/'EXPLICIT-$creationTime'"
+
+        pacman -Qqem > $pkgListDir/aur/"AUR-$creationTime.txt"
+        echo "Created explicitly installed AUR package listing at $pkgListDir/aur/'AUR-$creationTime'"
+
+        comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) > $pkgListDir/optional/"OPTIONAL-$creationTime.txt"
+        echo "Created installed optional dependency package listing at $pkgListDir/optional/'OPTIONAL-$creationTime.txt'"
+    fi
+fi
+
+unset creationTime
+unset pkgListDir
