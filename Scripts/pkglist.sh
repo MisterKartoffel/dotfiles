855de39a12015a391dd288316dfeef1f6e2de0b1
Felipe Duarte <176643461+MisterKartoffel@users.noreply.github.com>
2024-09-18 22:16:52 -0300

    Refactored pkglist.sh code and regenerated package listings

--- Scripts/pkglist.sh
+++ Scripts/pkglist.sh
@@ -1,46 +1,64 @@
 #!/bin/bash
 
-# WARNING: THIS SCRIPT IS REFERENCED
-# BY FULL PATH IN /etc/pacman.d/hooks/96-pkglistgen_post.hook
-
-creationTime="$(date --rfc-3339="seconds")"
 pkgListDir="$HOME/.config/pkglists"
 
-if [ ! -e "$pkgListDir" ]; then
-    mkdir -p $pkgListDir/{aur,repositories,optional,flatpak}
-    echo "Created $pkgListDir and subdirectories."
-elif [ -f "$pkgListDir" ]; then
-    echo "Error: $pkgListDir exists as a file."
-    exit -1
-fi
+function setupListings () {
+    local creationTime="$(date --rfc-3339="seconds")"
 
-if [ -e "$pkgListDir" ]; then
-    if [ ! -e "$pkgListDir/aur" ]; then
-        echo "Error: $pkgListDir/aur does not exist."
-        exit -1
-    elif [ ! -e "$pkgListDir/repositories" ]; then
-        echo "Error: $pkgListDir/repositories does not exist."
-        exit -1
-    elif [ ! -e "$pkgListDir/optional" ]; then
-        echo "Error: $pkgListDir/optional does not exist."
-        exit -1
-    elif [ ! -e "$pkgListDir/flatpak" ]; then
-        echo "Error: $pkgListDir/flatpak does not exist."
-        exit -1
-    else
-        pacman -Qqten > $pkgListDir/repositories/"EXPLICIT-$creationTime.txt"
-        echo "Created explicitly installed package listing at $pkgListDir/repositories/'EXPLICIT-$creationTime'"
+    case "$1" in
+        aur)
+            pacman -Qqem > "$pkgListDir"/"$1"/"AUR-$creationTime.txt"
+            echo "Created explicitly installed AUR package listing at $pkgListDir/$1/'AUR-$creationTime'"
+            ;;
 
-        pacman -Qqem > $pkgListDir/aur/"AUR-$creationTime.txt"
-        echo "Created explicitly installed AUR package listing at $pkgListDir/aur/'AUR-$creationTime'"
+        flatpak)
+            flatpak list --app > "$pkgListDir"/"$1"/"FLATPAK-$creationTime.txt"
+            echo "Created installed Flatpak apps listing at $pkgListDir/$1/'FLATPAK-$creationTime.txt'"
+            ;;
 
-        comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) > $pkgListDir/optional/"OPTIONAL-$creationTime.txt"
-        echo "Created installed optional dependency package listing at $pkgListDir/optional/'OPTIONAL-$creationTime.txt'"
+        optional)
+            comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) > "$pkgListDir"/"$1"/"OPTIONAL-$creationTime.txt"
+            echo "Created installed optional dependency package listing at $pkgListDir/$1/'OPTIONAL-$creationTime.txt'"
+            ;;
 
-        flatpak list --app > $pkgListDir/flatpak/"FLATPAK-$creationTime.txt"
-        echo "Created installed flatpak apps listing at $pkgListDir/flatpak/'FLATPAK-$creationTime.txt'"
-    fi
-fi
+        repositories)
+            pacman -Qqten > "$pkgListDir"/"$1"/"EXPLICIT-$creationTime.txt"
+            echo "Created explicitly installed package listing at $pkgListDir/$1/'EXPLICIT-$creationTime'"
+            ;;
 
-unset creationTime
+        *)
+            echo "Error: $1 undefined as a source."
+            exit 1
+            ;;
+    esac
+
+    unset creationTime
+}
+
+function setupDirectories () {
+    for source in "$@";
+    do
+        if [ -f "$pkgListDir" ]; then
+            echo "Error: $pkgListDir exists as a file."
+            exit 1
+        elif [ ! -e "$pkgListDir" ]; then
+            mkdir "$pkgListDir"
+            echo "Package listing master directory created at $pkgListDir"
+        fi
+
+        if [ -f "$pkgListDir"/"$source" ]; then
+            echo "Error: $pkgListDir/$source exists as a file."
+            exit 1
+        elif [ ! -e "$pkgListDir"/"$source" ]; then
+            mkdir -p "$pkgListDir"/"$source"
+            echo "Created $source listing directory at $pkgListDir/$source"
+            setupListings "$source"
+        fi
+    done
+}
+
+setupDirectories "aur" "flatpak" "optional" "repositories"
+
 unset pkgListDir
+unset setupDirectories
+unset setupListings
