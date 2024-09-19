f918f68d671a79ae47be7788ce741e78fc061ae6
Felipe Duarte <176643461+MisterKartoffel@users.noreply.github.com>
2024-09-19 00:31:59 -0300

    I lied

--- Scripts/pkglist.sh
+++ Scripts/pkglist.sh
@@ -8,24 +8,24 @@ creationTime="$(date --rfc-3339="seconds")"
 
 function setupListings () {
     case "$1" in
-        aur)
+        AUR)
             pacman -Qqem > "$pkgListDir"/"$1"/"AUR-$creationTime.txt"
-            echo -e "Created explicitly installed AUR package listing at $pkgListDir/$1/'AUR-$creationTime'\n"
+            echo -e "AUR listing: $pkgListDir/$1/'AUR-$creationTime'\n"
             ;;
 
-        flatpak)
+        Flatpak)
             flatpak list --app > "$pkgListDir"/"$1"/"FLATPAK-$creationTime.txt"
-            echo -e "Created installed Flatpak apps listing at $pkgListDir/$1/'FLATPAK-$creationTime.txt'\n"
+            echo -e "Flatpak listing: $pkgListDir/$1/'FLATPAK-$creationTime.txt'\n"
             ;;
 
-        optional)
+        Optional)
             comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) > "$pkgListDir"/"$1"/"OPTIONAL-$creationTime.txt"
-            echo -e "Created installed optional dependency package listing at $pkgListDir/$1/'OPTIONAL-$creationTime.txt'\n"
+            echo -e "Optional dependency listing: $pkgListDir/$1/'OPTIONAL-$creationTime.txt'\n"
             ;;
 
-        explicit)
+        Explicit)
             pacman -Qqten > "$pkgListDir"/"$1"/"EXPLICIT-$creationTime.txt"
-            echo -e "Created explicitly installed package listing at $pkgListDir/$1/'EXPLICIT-$creationTime'\n"
+            echo -e "Explicit install listing: $pkgListDir/$1/'EXPLICIT-$creationTime'\n"
             ;;
 
         *)
@@ -43,7 +43,7 @@ function setupDirectories () {
             exit 1
         elif [ ! -e "$pkgListDir" ]; then
             mkdir "$pkgListDir"
-            echo -e "Package listing master directory created at $pkgListDir\n"
+            echo -e "New package listing master directory: $pkgListDir\n"
         fi
 
         if [ -f "$pkgListDir"/"$source" ]; then
@@ -51,16 +51,11 @@ function setupDirectories () {
             exit 1
         elif [ ! -e "$pkgListDir"/"$source" ]; then
             mkdir -p "$pkgListDir"/"$source"
-            echo "Created $source listing directory at $pkgListDir/$source"
+            echo "New $source listing directory: $pkgListDir/$source"
         fi
 
         setupListings "$source"
     done
 }
 
-setupDirectories "aur" "flatpak" "optional" "explicit"
-
-unset pkgListDir
-unset creationTime
-unset setupListings
-unset setupDirectories
+setupDirectories "AUR" "Flatpak" "Optional" "Explicit"
