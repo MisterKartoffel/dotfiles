87a6f5a68ea553af5bffa7384034653d15fea5ff
Felipe Duarte <176643461+MisterKartoffel@users.noreply.github.com>
2024-09-18 22:25:33 -0300

    Added Shellcheck to none-ls

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -9,6 +9,7 @@ return {
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.biome,
+                null_ls.builtins.diagnostics.shellcheck,
             },
         })
     end,
