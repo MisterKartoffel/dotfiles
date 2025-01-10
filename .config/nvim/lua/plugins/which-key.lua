aaf57ffd1513db9a1d3dfcbe6eab9faa079a6720
Felipe Duarte <176643461+MisterKartoffel@users.noreply.github.com>
2025-01-09 21:22:51 -0300

    Which-key preset

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,6 +2,7 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
+        preset = "helix",
         keys = {
             scroll_down = "<C-j>",
             scroll_up = "<C-k>",
